83d26c8f525ad9250ea73dc60619d0ae86e88409
Arjithar8 <arjithar8@gmail.com>
2026-03-08 17:57:30 +0530

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,29 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="flex min-h-screen items-center justify-center">
+      <main className="p-8 rounded-3xl w-[26rem]">
+        <img src="/image-equilibrium.jpg" alt="equilibrium" className="rounded-xl"/>
+        <h1 className="text-3xl font-bold py-6">Equilibrium #3429</h1>
+
+        <p className="text-xl text-blue-500 font-thin">Our Equilibrium collection promotes balance and calm.</p>
+        <div className="flex flex-row justify-between py-4 text-xl">
+          <div className="flex gap-2 text-cyan-400">
+            <img src="/icon-ethereum.svg" alt="ETH" className="inline-block h-6"/>
+            <p className="font-bold">0.041 ETH</p>
+          </div>
+          <div className="flex gap-2 items-center text-blue-500">
+            <img src="/icon-clock.svg" alt="clock" className="inline-block w-6 h-6"/>
+            <p>3 days left</p>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+
+        <hr className="border-0 border-t border-blue-800" />
+        <footer className="flex items-center gap-4 pt-4 text-xl">
+          <img src="/image-avatar.png" alt="avatar" className="rounded-full w-12 border"/>
+          <div>
+          <span className="text-blue-500">Creation of</span>{" "}Jules Wyvern
+          </div>
+        </footer>
       </main>
     </div>
   );
